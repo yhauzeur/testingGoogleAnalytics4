edb3b30660fe620bd88d3b9e2a9f7565fb115598
blancometrics <blancometrics@gmail.com>
2024-05-09 22:29:59 -0500

    events in GA$ + GTM

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let h2 = document.querySelector('h2');
 const width = document.querySelector('#w');
 const height = document.querySelector('#h');
 
-title.innerText = "Apendiendo Javascript con Platzi";
+title.innerText = "Apendiendo Javascript con Platzi el motor de V8 junto con Google Tag Mananer y G. Analytics";
 
 // height.innerText = `${window.innerHeight} Pixel`;
 // width.innerText = ` ${window.innerWidth} Pixels`;
